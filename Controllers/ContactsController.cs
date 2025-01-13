21ff890fa7325129207f26c10f6d2678415b72f3
Iradium59 <samuel.definance003.jeu@gmail.com>
2025-01-13 15:18:55 +0100

    exercice 1

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciceMVC.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Je suis la page pour afficher les contacts...");
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Content($"Je suis la page pour afficher le contact avec l'ID : {id}...");
+        }
+
+        public IActionResult Add()
+        {
+            return Content("Je suis la page pour ajouter un contact...");
+        }
+    }
+}
